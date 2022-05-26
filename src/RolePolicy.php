3cea24c5d844d4b833083c7dfa2d45193151dfac
Stanislav Kabin <ioffline@make-it.app>
2022-05-26 16:56:05 +0300

    console commit

--- src/RolePolicy.php
+++ src/RolePolicy.php
@@ -2,7 +2,6 @@
 
 namespace MakeIT\UserRoles;
 
-use MakeIT\UserRoles\Role;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -12,112 +11,139 @@ class RolePolicy
 
     /**
      * Determine whether the user can view any models.
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return bool
      */
-    public function viewAny(User $user)
+    public function viewAny( User $user ): bool
     {
         return true;
     }
 
     /**
      * Determine whether the user can view the model.
-     * @param  \App\Models\User  $user
-     * @param  \MakeIT\UserRoles\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function view(User $user, Role $role)
+    public function view( User $user, Role $role ): bool
     {
         return true;
     }
 
     /**
      * Determine whether the user can create models.
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return bool
      */
-    public function create(User $user)
+    public function create( User $user ): bool
     {
-        return $user->hasRole([ 'super', 'admin' ]);
+        return $user->hasRole( [
+            'super',
+            'admin',
+        ] );
     }
 
     /**
      * Determine whether the user can update the model.
-     * @param  \App\Models\User  $user
-     * @param  \MakeIT\UserRoles\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function update(User $user, Role $role)
+    public function update( User $user, Role $role ): bool
     {
-        return $user->hasRole([ 'super', 'admin' ]);
+        return $user->hasRole( [
+            'super',
+            'admin',
+        ] );
     }
 
     /**
      * Determine whether the user can delete the model.
-     * @param  \App\Models\User  $user
-     * @param  \MakeIT\UserRoles\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function delete(User $user, Role $role)
+    public function delete( User $user, Role $role ): bool
     {
-        return $user->hasRole([ 'super', 'admin' ]);
+        return $user->hasRole( [
+            'super',
+            'admin',
+        ] );
     }
 
     /**
      * Determine whether the user can restore the model.
-     * @param  \App\Models\User  $user
-     * @param  \MakeIT\UserRoles\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function restore(User $user, Role $role)
+    public function restore( User $user, Role $role ): bool
     {
-        return $user->hasRole([ 'super', 'admin' ]);
+        return $user->hasRole( [
+            'super',
+            'admin',
+        ] );
     }
 
     /**
      * Determine whether the user can permanently delete the model.
-     * @param  \App\Models\User  $user
-     * @param  \MakeIT\UserRoles\Role  $role
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function forceDelete(User $user, Role $role)
+    public function forceDelete( User $user, Role $role ): bool
     {
-        return $user->hasRole([ 'super', 'admin' ]);
+        return $user->hasRole( [
+            'super',
+            'admin',
+        ] );
     }
 
     /**
      * Determine whether the user can attach a User to a Role.
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Podcast  $podcast
-     * @param  \App\Models\Tag  $tag
-     * @return mixed
+     * @param User $user
+     * @param Role $role
+     * @param User $User
+     * @return bool
      */
-    public function attachUser( User $user, Role $role, User $User )
+    public function attachUser( User $user, Role $role, User $User ): bool
     {
-        return $user->hasRole([ 'super', 'admin', 'support' ]) || $role->name == 'user';
+        return $user->hasRole( [
+                'super',
+                'admin',
+                'support',
+            ] ) || $role->name == 'user';
     }
 
     /**
      * Determine whether the user can detach a User from a Role.
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Podcast  $podcast
-     * @param  \App\Models\Tag  $tag
-     * @return mixed
+     * @param User $user
+     * @param Role $role
+     * @param User $User
+     * @return bool
      */
-    public function detachUser( User $user, Role $role, User $User )
+    public function detachUser( User $user, Role $role, User $User ): bool
     {
-        return ( $user->hasRole([ 'super', 'admin', 'support' ]) || ($role->name != 'user' && $role != 'super' ) ) ;
+        return ( $user->hasRole( [
+                'super',
+                'admin',
+                'support',
+            ] ) || ( $role->name != 'user' && $role != 'super' ) );
     }
 
     /**
      * Determine whether the user can attach any User to the Role.
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Podcast  $podcast
-     * @return mixed
+     * @param User $user
+     * @param Role $role
+     * @return bool
      */
-    public function attachAnyUser( User $user, Role $role )
+    public function attachAnyUser( User $user, Role $role ): bool
     {
-        return $user->hasRole([ 'super', 'admin', 'support' ]) || $role->name == 'user';
+        return $user->hasRole( [
+                'super',
+                'admin',
+                'support',
+            ] ) || $role->name == 'user';
     }
 
 }
